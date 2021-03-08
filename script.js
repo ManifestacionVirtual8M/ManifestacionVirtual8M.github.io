22a4e860803c369bf871eaea3d1b067dce7ba4a5
daladno13 <slickindub@gmail.com>
2021-03-08 23:05:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -271,7 +271,7 @@ function addMarker2() {
     var rand = Math.floor(Math.random() * cars.length);
 
     var lt = randomFloat(40.408, 40.418);
-    var ln = randomFloat(-3.695, -3.691);
+    var ln = randomFloat(-3.697, -3.688);
     var marker = new google.maps.Marker({
         position: {
             lat: lt,
@@ -296,7 +296,7 @@ function addMarker2() {
 
 function addMarker() {
     var lt = randomFloat(40.408, 40.418);
-    var ln = randomFloat(-3.695, -3.691);
+    var ln = randomFloat(-3.697, -3.688);
     var marker = new google.maps.Marker({
         position: {
             lat: lt,
